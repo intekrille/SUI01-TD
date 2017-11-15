ac57e520ccdf6bbd3570df4498a6eda68ed373ec
bajjskorven1234 <chiaszurei888_9@hotmail.com>
2017-11-15 13:30:36 +0100

    Healthbar
    This is healthbar

--- New Unity Project 1/Assets/Tower/Enemy.cs
+++ New Unity Project 1/Assets/Tower/Enemy.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
-    public float health = 1f;
+    public float startHealth = 100f;
+    private float health;
     public float speed = 5f;
+
+    [Header("Unity stuff")]
+    public Image healthBar;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        health = startHealth;
 	}
 	
 	// Update is called once per frame
@@ -20,6 +26,7 @@ public class Enemy : MonoBehaviour {
     public void TakeHit(float damage)
     {
         health -= damage;
+        healthBar.fillAmount = health / startHealth;
         if (health <= 0f)
         {
             Destroy(gameObject);
